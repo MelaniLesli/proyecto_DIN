256a7a9cc390933075daa050536cd5853f6a69b7
MelaniLesli <melani.ramos@educa.madrid.org>
2023-03-31 00:32:13 +0200

    Estructura inicial (Formulario): * Se añade nuevo Activity, y se hace programa la transición entre pantalla principal y el formulario a través del botón 'Empezar'. * Se añaden los campos en formulario:  - Nombres y Apellidos  - Nombre Usuario  - Edad  - Correo Electrónico

--- app/src/main/java/com/example/doitnow/MainActivity.java
+++ app/src/main/java/com/example/doitnow/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.doitnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*Cambio de Pantalla al formulario Registro*/
+    public void onClick(View view) {
+        Intent irForm = new Intent(MainActivity.this, FormularioActivity.class);
+        startActivity(irForm);
     }
 }
